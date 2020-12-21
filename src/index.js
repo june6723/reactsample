18e39883fd0447655be9a9be184d5fef1e9ef58d
june6723 <june6723@naver.com>
2020-12-21 23:14:19 +0900

    Last challenge

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App2 from "./App2";
+import App from "./Components/App";
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App2 />, rootElement);
